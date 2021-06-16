c8a2f84ae15435e6474e23d769d621f64a00cd4a
Corey Gardner <coreygardner@Coreys-MacBook-Pro.local>
2021-06-16 13:40:25 -0400

    monthly finished

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
+
   def home
     @daily_workout = DailyWorkout.last
     @monthly_workout = MonthlyWorkout.last
   end
+
+  # TODO make sure monthly workout has something attached before rendeirng button and you can download it
+  # push to remote
+
 end
--- app/models/monthly_workout.rb
+++ app/models/monthly_workout.rb
@@ -1,4 +1,3 @@
 class MonthlyWorkout < ApplicationRecord
   has_one_attached :monthly_plan
-  # todo validate attachment presence
 end
